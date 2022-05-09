3d99035fa37b17d4bea699e12e42fd094152d813
hasibmiraz <hasibmiraz09@gmail.com>
2022-05-09 23:37:43 +0600

    Initial setup and navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Working tailwind</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,19 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#0FCFEC',
+          secondary: '#0FCFEC',
+          accent: '#3A4256',
+          neutral: '#3d4451',
+          'base-100': '#ffffff',
+        },
+      },
+      'light',
+    ],
+  },
 };
