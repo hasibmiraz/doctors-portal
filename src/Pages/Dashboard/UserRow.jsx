4c22c0ca50da61dd44ca5256cf4f37f9d0f8ba11
hasibmiraz <hasibmiraz09@gmail.com>
2022-05-16 03:33:25 +0600

    Make a user admin

---
+++ src/Pages/Dashboard/UserRow.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const UserRow = ({ user, index, refetch }) => {
+  const { email, role } = user;
+
+  const makeAdmin = () => {
+    fetch(`http://localhost:5000/user/admin/${email}`, {
+      method: 'PUT',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        toast.success(`${email} is now an admin!`);
+        refetch();
+      });
+  };
+  return (
+    <tr>
+      <th>{index + 1}</th>
+      <td>{user.email}</td>
+      <td>
+        {role === 'admin' ? (
+          <p className="text-white bg-green-600 w-1/4 text-center rounded-md px-3 py-2">
+            Admin
+          </p>
+        ) : (
+          <button onClick={makeAdmin} className="btn btn-sm text-white">
+            Make Admin
+          </button>
+        )}
+      </td>
+      <td>
+        <button className="btn btn-square btn-outline">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default UserRow;
--- src/Pages/Dashboard/Users.jsx
+++ src/Pages/Dashboard/Users.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import Spinner from '../Shared/Spinner';
 import Title from '../Title/Title';
+import UserRow from './UserRow';
 
 const Users = () => {
-  const { data: users, isLoading } = useQuery('users', () =>
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery('users', () =>
     fetch('http://localhost:5000/user', {
       method: 'GET',
       headers: {
@@ -34,31 +39,7 @@ const Users = () => {
           <tbody>
             {/* <!-- row 1 --> */}
             {users.map((user, i) => (
-              <tr key={user._id}>
-                <th>{i + 1}</th>
-                <td>{user.email}</td>
-                <td>
-                  <button class="btn btn-sm text-white">Make Admin</button>
-                </td>
-                <td>
-                  <button class="btn btn-square btn-outline">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
-                </td>
-              </tr>
+              <UserRow key={user._id} user={user} index={i} refetch={refetch} />
             ))}
           </tbody>
         </table>
