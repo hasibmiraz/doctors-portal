42103f5380f36f8160b62b8a15e8c02dff58d07f
hasibmiraz <hasibmiraz09@gmail.com>
2022-05-21 21:13:25 +0600

    Added server side url

--- src/Pages/Dashboard/CheckoutForm.jsx
+++ src/Pages/Dashboard/CheckoutForm.jsx
@@ -13,7 +13,7 @@ const CheckoutForm = ({ appointment }) => {
   const [clientSecret, setClientSecret] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/create-payment-intent', {
+    fetch('https://gentle-plains-18586.herokuapp.com/create-payment-intent', {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
@@ -76,7 +76,7 @@ const CheckoutForm = ({ appointment }) => {
         transactionId: paymentIntent.id,
       };
 
-      fetch(`http://localhost:5000/booking/${_id}`, {
+      fetch(`https://gentle-plains-18586.herokuapp.com/booking/${_id}`, {
         method: 'PATCH',
         headers: {
           'content-type': 'application/json',
